b967c7af23433f5370914bdfdf6440b03918c9ce
Marcus Paulo <mpaulobr@gmail.com>
2017-02-17 00:15:58 -0200

    Commits iniciais do projeto de Cerveja (Curso)

---
+++ src/main/java/com/mp/brewer/config/init/AppInitializer.java
@@ -0,0 +1,42 @@
+package com.mp.brewer.config.init;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.mp.brewer.config.WebConfig;
+
+/**
+ * Classe Responsável por Configurar a Inicialização do Projeto, 
+ * sem a necessidade do web.xml
+ * @author marcus
+ *
+ */
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] {WebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+	
+	//Resolvendo possíveis problemas de Acentuação
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return new Filter[] { characterEncodingFilter};
+	}
+
+}
---
+++ src/main/java/com/mp/brewer/controller/CerverjasController.java
@@ -0,0 +1,31 @@
+package com.mp.brewer.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.mp.brewer.model.Cerveja;
+
+@Controller
+public class CerverjasController {
+
+	@RequestMapping("/cervejas/novo")
+	public String novo(Cerveja cerveja) {
+		return "cerveja/CadastroCerveja";
+	}
+	
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+			return novo(cerveja);
+		}
+		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
+		System.out.println("Cadastrar " + cerveja.getSku() + " - " + cerveja.getNome());
+		return "redirect:/cervejas/novo";
+	}
+}
